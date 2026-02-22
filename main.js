ec1a5ee4b4bf0a6288357c32fc559865c9c8b255
jegan722005 <jegan722005@gmail.com>
2026-02-22 20:32:20 +0530

    update add main.js

---
+++ main.js
@@ -0,0 +1,60 @@
+// click me btu
+function showAlert() {
+    document.getElementById("customAlert").style.display = "block";
+}
+function closeAlert() {
+    document.getElementById("customAlert").style.display = "none";
+}
+
+// add my photo
+
+let myphoto = document.querySelector("#home > .right-side > img");
+myphoto.src = "jegan.jpg"
+
+// skills
+
+let skill1 = document.querySelector("#skill > .skills > .span1")
+skill1.innerHTML = `Java`
+let skill2 = document.querySelector("#skill > .skills > .span2")
+skill2.innerHTML = `JDBC Connectivity`
+let skill3 = document.querySelector("#skill > .skills > .span3")
+skill3.innerHTML = `Oracle SQL`
+let skill4 = document.querySelector("#skill > .skills > .span4")
+skill4.innerHTML = `HTML`
+let skill5 = document.querySelector("#skill > .skills > .span5")
+skill5.innerHTML = `CSS`
+let skill6 = document.querySelector("#skill > .skills > .span6")
+skill6.innerHTML = `GitHub`
+
+//resume btn
+
+let downloadBtn = document.getElementById("resumeBtn");
+
+downloadBtn.addEventListener("click", () => {
+    let anchor = document.createElement("a")
+    anchor.href = "JeganResume.pdf";
+    anchor.download = "JeganResume.pdf"
+    document.body.appendChild(anchor); // add to DOM
+    anchor.click(); // trigger download
+    document.body.removeChild(anchor); // clean up
+})
+
+//View Resume
+
+let resumeView=document.getElementById("resumeView");
+
+resumeView.addEventListener("click",()=>{
+
+    let anchor=document.createElement("a");
+    anchor.href="https://drive.google.com/file/d/1dTZhUaP4LT8NIW6ULxuSa43IhCIyAkSa/view?usp=sharing"
+    
+    anchor.target="_blank";
+})
+
+
+
+
+
+// footer 
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
